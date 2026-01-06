23111cf53426c19af527549d83e50e8bec72200d
Asifujjaman Opi <asifujjaman.opi@bracits.com>
2026-01-06 16:07:52 +0600

    update Spring Boot version, enhance Student model with Lombok, and add new endpoints in StudentController

--- src/main/java/net/javaguides/springboot_rest_api/bean/Student.java
+++ src/main/java/net/javaguides/springboot_rest_api/bean/Student.java
@@ -1,41 +1,14 @@
 package net.javaguides.springboot_rest_api.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Student {
   private int id;
   private String firstName;
   private String lastName;
-
-  public Student(int id, String firstName, String lastName) {
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-  public Student() {
-
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
 }
--- src/main/java/net/javaguides/springboot_rest_api/controller/StudentController.java
+++ src/main/java/net/javaguides/springboot_rest_api/controller/StudentController.java
@@ -1,18 +1,63 @@
 package net.javaguides.springboot_rest_api.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import net.javaguides.springboot_rest_api.bean.Student;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("student")
 public class StudentController {
 
+  // http://localhost:8080/student
   @GetMapping("student")
-  public Student getStudent() {
+  public ResponseEntity<Student> getStudent() {
     Student student = new Student(
-      1, "Asif", "Jaman"
+        1, "Asif", "Jaman"
     );
 
-    return student;
+    return ResponseEntity.ok(student);
+  }
+
+  // http://localhost:8080/students
+  @GetMapping()
+  public ResponseEntity<List<Student>> getStudents() {
+
+    List<Student> students = new ArrayList<>();
+
+    students.add(new Student(
+        1, "Ramesh", "Fadatare"
+    ));
+
+    students.add(new Student(
+        2, "Umesh", "Fadatare"
+    ));
+
+    students.add(new Student(
+        3, "Ram", "Jadhav"
+    ));
+
+    students.add(new Student(
+        4, "Sanjay", "Pawar"
+    ));
+
+    return ResponseEntity.ok(students);
+  }
+
+  // http://localhost:8080/students/1/ramesh/fadatare
+  @GetMapping("{id}/{first-name}/{last-name}")
+  public ResponseEntity<Student> studentPathVariable(@PathVariable int id, @PathVariable("first-name") String firstName, @PathVariable("last-name") String lastName) {
+    return ResponseEntity.ok(new Student(id, "Asif", "Jaman"));
+  }
+
+  //  http://localhost:8080/students/query?id=1&firstName=Ramesh&lastName=Fadatare
+  @GetMapping("query")
+  public ResponseEntity<Student> studentRequestVariable(@RequestParam int id,
+                                        @RequestParam String firstName,
+                                        @RequestParam String lastName){
+    return ResponseEntity.ok(new Student(id, firstName, lastName));
   }
 }
