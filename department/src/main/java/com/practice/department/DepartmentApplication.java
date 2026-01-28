370478d1f74f8ab4acb7ab97f7afe79861be41ae
Asifujjaman Opi <asifujjaman.opi@bracits.com>
2026-01-28 19:04:41 +0600

    update to Spring Boot 3 and Spring Cloud 2022.0.1, replace @EnableEurekaClient with @EnableDiscoveryClient, and comment out Zipkin dependency

--- department/src/main/java/com/practice/department/DepartmentApplication.java
+++ department/src/main/java/com/practice/department/DepartmentApplication.java
@@ -3,11 +3,12 @@ package com.practice.department;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class DepartmentApplication {
 	@Bean
 	public ModelMapper modelMapper() {
