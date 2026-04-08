a2681227cb9056c0fe6a05ad836244bd07ed895d
Asifujjaman Opi <asifujjaman.opi@bracits.com>
2026-04-08 17:48:08 +0600

    initialize project

--- springboot-kafka-real-world-project/kafka-consumer-database/src/main/java/com/practice/springboot/repository/WikimediaDataRepository.java
+++
@@ -1,8 +0,0 @@
-package com.practice.springboot.repository;
-
-import com.practice.springboot.entity.WikimediaData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
-
-}
