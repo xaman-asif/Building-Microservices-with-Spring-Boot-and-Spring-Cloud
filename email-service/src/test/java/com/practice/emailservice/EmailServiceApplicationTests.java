17fd2a82b614b21f7b6569100d064d71db9609a1
Asifujjaman Opi <asifujjaman.opi@bracits.com>
2026-04-13 16:50:16 +0600

    refactor package structure and implement Order service with Kafka integration

---
+++ email-service/src/test/java/com/practice/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
